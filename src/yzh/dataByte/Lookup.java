77cf7edef515d62a5b05034b8f26945c40382c5c
zhyan5726 <zhyan5726@fiberhome.com>
2016-12-28 13:40:23 +0800

    [N]

---
+++ src/yzh/dataByte/Lookup.java
@@ -0,0 +1,38 @@
+package yzh.dataByte;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Connection;
+
+public class Lookup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String dbUrl = "jdbc:odbc:people";
+		String user = "";
+		String password = "";
+		try{
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			Connection connection =(Connection) DriverManager.getConnection(dbUrl,user,password);
+			java.sql.Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT FIRST,LAST,EMAIL"
+					+ "FROM people.csv people"
+					+ "WHERE"
+					+ "(LAST ='"+args[0]+"')"
+					+ "AND(EMAIL Is Not Null)"
+					+ "ORDER BY FIRST");
+			while(resultSet.next()){
+				System.out.println(resultSet.getString("Last")+ ","
+						+resultSet.getString("first")+":"
+						+resultSet.getString("EMAIL"));
+			}
+			statement.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			
+			
+		}
+	}
+
+}
